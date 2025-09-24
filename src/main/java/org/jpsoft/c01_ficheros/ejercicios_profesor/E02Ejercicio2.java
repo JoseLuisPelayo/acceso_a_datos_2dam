cc1d205fec2545d267fcdb8757603533fde2c84f
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-09-24 20:26:22 +0200

    Add: Ejercicio2 E02Ejercicio2 mejorado

--- src/main/java/org/jpsoft/c01_ficheros/ejercicios_profesor/E02Ejercicio2.java
+++ src/main/java/org/jpsoft/c01_ficheros/ejercicios_profesor/E02Ejercicio2.java
@@ -90,15 +90,15 @@ public class E02Ejercicio2 {
 // *************************************** NUEVA APROXIMACION ****************************************************************
 
             List<String> textWords = splitString(entryFileTextToString());
-            Comparator<Map.Entry<String, Integer>> comparator = Map.Entry.comparingByValue(Comparator.reverseOrder());
+            Comparator<Map.Entry<String, Integer>> comparatorByValue = Map.Entry.comparingByValue(Comparator.reverseOrder());
 
 
-            LinkedHashMap<String, Integer> newTry = textWords.stream().collect(Collectors.toMap(
+            LinkedHashMap<String, Integer> newTry = textWords.stream()
+                    .collect(Collectors.groupingBy(
                             w -> w,
-                            v -> 1,
-                            Integer::sum,
-                            LinkedHashMap::new))
-                    .entrySet().stream().sorted(comparator).collect(Collectors.toMap(
+                            Collectors.summingInt(w -> 1)
+                    ))
+                    .entrySet().stream().sorted(comparatorByValue).collect(Collectors.toMap(
                             Map.Entry::getKey,
                             Map.Entry::getValue,
                             (e1, e2) -> e1,
